3d81f0dabbf7727b99011d13a395e9ed3273d892
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-04-04 17:16:25 +0500

    order models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from ..users.models import User
+from ..catalog.models import Product
+
+class StatusOrder(models.TextChoices):
+    PENDING = 'pending', 'Pending' # — hali ko‘rilmagan
+    CONFIRMED = 'confirmed', 'Confirmed' # — tasdiqlangan
+    DELIVERED = 'delivered', 'Delivered' # — yetkazilgan
+    CANCELLED = 'cancelled', 'Cancelled' # — bekor qilingan
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete='CASCADE')
+    full_name = models.CharField(max_length=32)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=64)
+    house = models.CharField(max_length=32)
+    apartment = models.CharField(max_length=32)
+    comment = models.CharField(max_length=500)
+    total_price = models.FloatField()
+    status = models.CharField(max_length=20, choices=StatusOrder.choices, default=StatusOrder.PENDING)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='order_items', on_delete='CASCADE')
+    product = models.ForeignKey(Product, related_name='order_items', on_delete='CASCADE')
+    quantity = models.IntegerField()
+    price = models.FloatField()
