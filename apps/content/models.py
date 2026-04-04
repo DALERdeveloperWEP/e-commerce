e317bc849f207f571992912e11b694be6a31780f
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-04-04 19:30:56 +0500

    store and content medols

--- apps/content/models.py
+++ apps/content/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='media/content/')
+    short_description = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=128)
+    requirements = models.CharField(max_length=128)
+    responsibilities = models.CharField(max_length=128)
+    conditions = models.CharField(max_length=128)
+    phone = models.CharField(max_length=15)
+    is_active = models.BooleanField(default=True)
--- apps/stores/models.py
+++ apps/stores/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=128)
+    phone = models.CharField(max_length=15)
+    latitude = models.CharField(max_length=255)
+    longitude = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
