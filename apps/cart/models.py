931c0856ade6beb1217a71692373b768f35ba1a0
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-04-08 23:08:25 +0500

    done cart

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -12,4 +12,6 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
+    quantity = models.PositiveIntegerField(default=1)
+    class Meta:
+        unique_together = ['cart', 'product']
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,17 +1,34 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
-from .models import CartItem
+from .models import CartItem, Cart
 from .serializers import CartItemSerializer
+from ..catalog.permissions import IsOwnerOrReadOnly, IsUserOrReadOnly
 
 
 class CartItemViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsUserOrReadOnly, IsOwnerOrReadOnly]
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
     
-    def perform_create(self, serializer):
-        
-    
     def get_queryset(self):
-        return self.queryset.filter(cart__user=self.request.user)
+        return CartItem.objects.filter(cart__user=self.request.user)
+    
+    
+    def create(self, request, *args, **kwargs):
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+
+        product_id = request.data.get("product")
+
+        cart_item, created = CartItem.objects.get_or_create(
+            cart=cart,
+            product_id=product_id,
+        )
+
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save()
+
+        serializer = self.get_serializer(cart_item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
